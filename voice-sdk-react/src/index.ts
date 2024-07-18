f56fb007da6fce3d11ab21f1a179d884e9a41cb4
Christian Stuff <christian@daily.co>
2024-07-18 11:57:22 +0200

    add VoiceClientAudio component

--- voice-sdk-react/src/index.ts
+++ voice-sdk-react/src/index.ts
@@ -1,3 +1,4 @@
+export { VoiceClientAudio } from "./VoiceClientAudio";
 export { VoiceClientProvider } from "./VoiceClientProvider";
 export { useVoiceClient } from "./useVoiceClient";
 export { useVoiceClientEvent } from "./useVoiceClientEvent";
