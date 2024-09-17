6cf5e58c69517d7938e306f2bc11ee982b9ebfb2
Christian Stuff <christian@daily.co>
2024-09-17 14:51:03 +0200

    deprecate pre-0.2.0 voice react hooks & components

--- rtvi-client-react/src/index.ts
+++ rtvi-client-react/src/index.ts
@@ -1,9 +1,63 @@
-export { RTVIClientAudio } from "./RTVIClientAudio";
-export { RTVIClientVideo } from "./RTVIClientVideo";
-export { VoiceVisualizer } from "./VoiceVisualizer";
-export { RTVIClientProvider } from "./RTVIClientProvider";
-export { useRTVIClient } from "./useRTVIClient";
-export { useRTVIClientEvent } from "./useRTVIClientEvent";
-export { useRTVIClientMediaDevices } from "./useRTVIClientMediaDevices";
-export { useRTVIClientMediaTrack } from "./useRTVIClientMediaTrack";
-export { useRTVIClientTransportState } from "./useRTVIClientTransportState";
+import { RTVIClientAudio } from "./RTVIClientAudio";
+import { RTVIClientVideo } from "./RTVIClientVideo";
+import { RTVIClientProvider } from "./RTVIClientProvider";
+import { VoiceVisualizer } from "./VoiceVisualizer";
+import { useRTVIClient } from "./useRTVIClient";
+import { useRTVIClientEvent } from "./useRTVIClientEvent";
+import { useRTVIClientMediaDevices } from "./useRTVIClientMediaDevices";
+import { useRTVIClientMediaTrack } from "./useRTVIClientMediaTrack";
+import { useRTVIClientTransportState } from "./useRTVIClientTransportState";
+
+/**
+ * @deprecated Use RTVIClientAudio instead.
+ */
+const VoiceClientAudio = RTVIClientAudio;
+/**
+ * @deprecated Use RTVIClientVideo instead.
+ */
+const VoiceClientVideo = RTVIClientVideo;
+/**
+ * @deprecated Use RTVIClientProvider instead.
+ */
+const VoiceClientProvider = RTVIClientProvider;
+/**
+ * @deprecated Use useRTVIClient instead.
+ */
+const useVoiceClient = useRTVIClient;
+/**
+ * @deprecated Use useRTVIClientEvent instead.
+ */
+const useVoiceClientEvent = useRTVIClientEvent;
+/**
+ * @deprecated Use useRTVIClientMediaDevices instead.
+ */
+const useVoiceClientMediaDevices = useRTVIClientMediaDevices;
+/**
+ * @deprecated Use useRTVIClientMediaTrack instead.
+ */
+const useVoiceClientMediaTrack = useRTVIClientMediaTrack;
+/**
+ * @deprecated Use useRTVIClientTransportState instead.
+ */
+const useVoiceClientTransportState = useRTVIClientTransportState;
+
+export {
+  RTVIClientAudio,
+  RTVIClientVideo,
+  VoiceVisualizer,
+  RTVIClientProvider,
+  useRTVIClient,
+  useRTVIClientEvent,
+  useRTVIClientMediaDevices,
+  useRTVIClientMediaTrack,
+  useRTVIClientTransportState,
+  // deprecated since 0.2.0
+  VoiceClientAudio,
+  VoiceClientVideo,
+  VoiceClientProvider,
+  useVoiceClient,
+  useVoiceClientEvent,
+  useVoiceClientMediaDevices,
+  useVoiceClientMediaTrack,
+  useVoiceClientTransportState,
+};
